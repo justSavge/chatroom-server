0032a7d569272bed86d8b457a210f324214f152d
lijun <jun.li@chiefclouds.com>
2024-12-16 22:51:20 +0800

    2024/12/8更新

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 const http = require("http").createServer(app);
 const io = new Server(http, {
   cors: {
@@ -11,21 +14,27 @@ const io = new Server(http, {
 });
 let count = 0;
 let userName = "";
-app.post("/login", (req, res) => {
+let remember;
+app.post("/login", async (req, res) => {
+  // userName = req.body.userName;
+  console.log("login在运行了", req.body);
   // TODO:对数据库操作，验证数据
   userName = req.body.userName;
-  console.log("也是有http");
+  remember = req.body.remember;
+  res.send(JSON.stringify({ isSuccess: true }));
 });
 app.post("/register", (req, res) => {
   console.log("也是有http了");
 });
 // 登录以后建立ws连接，可以正确得到用户名
-io.on("connect", (scoket) => {
+io.on("connect", (socket) => {
   count++;
   console.log("连上了", count);
-  scoket.on("message", (message) => {
+  console.log("post连上了", userName, remember);
+  socket.emit("postUserNameForEveryone", { userName, remember });
+  socket.on("message", (message) => {
     console.log(message);
-    io.emit("messageHuman", { ...message, userName });
+    io.emit("messageHuman", { ...message, userName, remember });
   });
 });
 
